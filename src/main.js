17a2d3f0c5ef79eab0f31127941872e1765e886b
Fabrizio <u201914181@upc.edu.pe>
2025-06-07 22:38:40 -0500

    feat(dependencies): add primevue dependencies in main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import Primevue from 'primevue/config'
+import Material from '@primeuix/themes/material'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import i18n from "./i18n/index.js";
+import {SelectButton} from "primevue";
+
 const app = createApp(App);
 
-app.mount('#app')
+app
+    .use(Primevue,{theme:{preset:Material},ripple:true})
+    .use(i18n)
+    .component('pv-select-button',SelectButton)
+    .mount('#app')
+
