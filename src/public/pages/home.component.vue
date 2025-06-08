cecd7eec6c6a43bc61c6e6494077a6b6df892990
Fabrizio <u201914181@upc.edu.pe>
2025-06-08 01:06:23 -0500

    feat(home): add home page

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<template>
+  <div class="flex flex-column">
+    <h1>Eventify's Home</h1>
+    <h2>Welcome to Eventify</h2>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
